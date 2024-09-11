f7ee6ef5c5dee4ce09d6ca3cf7237e3297992023
Pranam Shetty <prxshetty@gmail.com>
2024-09-11 12:49:24 -0400

    page updates

--- next.config.js
+++ next.config.js
@@ -4,10 +4,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  assetPrefix: '/prxshetty.github.io',
+  basePath: '/prxshetty.github.io'
 }
 if (process.env.NODE_ENV === 'production') {
   nextConfig.assetPrefix = '/prxshetty.github.io'
   nextConfig.basePath = '/prxshetty.github.io'
 }
+
 
 module.exports = nextConfig
