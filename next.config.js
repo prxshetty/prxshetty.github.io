3d515681c4cc95a81542d7e69d6217171dd28fba
Pranam Shetty <prxshetty@gmail.com>
2024-09-11 11:37:26 -0400

    github pages deployment code:

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: '/prxshetty.github.io/',
-  basePath: '/prxshetty.github.io',
+  images: {
+    unoptimized: true,
+  },
+}
+if (process.env.NODE_ENV === 'production') {
+  nextConfig.assetPrefix = '/prxshetty.github.io'
+  nextConfig.basePath = '/prxshetty.github.io'
 }
 
 module.exports = nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,4 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Pranam Prakash Shetty - Portfolio',
-  description: 'Data Science Student Portfolio',
-}
 
 export default function RootLayout({
   children,
