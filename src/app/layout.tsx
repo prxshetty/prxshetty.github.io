f42fd10465a36e99e1a56d906a579e13efffb10e
Pranam Shetty <prxshetty@gmail.com>
2024-10-15 00:00:19 -0400

    analytics tag update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,28 @@
 import '@/styles/globals.css';
-
+import Script from 'next/script';
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-45MEB3F176"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-45MEB3F176');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
-  );
+  )
 }
